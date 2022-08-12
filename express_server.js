12840ca64e7e92843f9f694e697dd2d45ba28381
daniscodes <dan97@bell.net>
2022-08-12 15:49:27 -0400

    Encrypted cookies

--- express_server.js
+++ express_server.js
@@ -1,264 +1,241 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
+const express = require('express');
 const bodyParser = require("body-parser");
+const cookieSession = require('cookie-session');
 const app = express();
-const bcrypt = require("bcryptjs");
-
 const PORT = 8080;
 
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser())
-
-function generateRandomString() {
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let str = ''
-
-  for (let i = 0; i < 6; i++) {
-    str += characters[Math.floor(Math.random() * characters.length)];
-  }
-  return str;
-}
-
-const urlsForUser = function (id) {
-  let userURL = {};
-  for (let key in urlDatabase) {
-    if (urlDatabase[key].userID === id) {
-      userURL[key] = urlDatabase[key];
-    }
-  }
-  return userURL;
-};
-
-const isUserInDatabase = function (userEmail) {
-  for (const key in users) {
-    if (users[key].email === userEmail) {
-      return true;
-    }
-  }
-  return false;
-};
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieSession({
+  name: 'session',
+  keys: ['LHL', 'Tiny', 'App'],
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}));
 
 const urlDatabase = {
   b6UTxQ: {
     longURL: "https://www.tsn.ca",
-    userID: "aJ48lW",
+    userID: "userRandomID"
   },
   i3BoGr: {
     longURL: "https://www.google.ca",
-    userID: "aJ48lW",
+    userID: "userRandomID"
   },
+  asdfqw: {
+    longURL: "https://www.google.ca",
+    userID: "user2RandomID"
+  }
 };
-
 const users = {
-  userRandomID: {
+  "userRandomID": {
     id: "userRandomID",
     email: "user@example.com",
-    password: bcrypt.hashSync("purple-monkey-dinosaur")
+    password: bcrypt.hashSync("test1")
   },
-  user2RandomID: {
+  "user2RandomID": {
     id: "user2RandomID",
     email: "user2@example.com",
-    password: bcrypt.hashSync("dishwasher-funk")
-  },
+    password: bcrypt.hashSync("test2")
+  }
 };
 
-app.use(express.urlencoded({ extended: true }));
+const isUserEmailInDatabase = function(userEmail) {
+  for (const key in users) {
+    if (users[key].email === userEmail) {
+      return users[key];
+    }
+  }
+  return false;
+};
+const generateRandomString = function() {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+};
 
-// root route
-app.get("/", (req, res) => {
-  if (users[req.cookies.user_id]) {
+// root page
+app.get('/', (req, res) => {
+  if (req.session.user_id) {
     res.redirect('/urls');
     return;
   }
-  res.redirect('/login');
 });
 
-// available urls in database
+// return the urls available in the database
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
-
+// user logout page
+app.post('/logout', (req, res) => {
+  eq.session = null;
+  res.redirect('/urls');
+});
 // user login page
 app.get("/login", (req, res) => {
-  if (users[req.cookies.user_id]) {
+  // !!! subject to change
+  if (req.session.user_id) {
     res.redirect('/urls');
     return;
   }
-  const templateVars = { user: users[req.cookies.user_id] };
+  const templateVars = { urls: urlDatabase, user: users[req.session.user_id] };
   res.render("login", templateVars);
 });
 
-// login existing user
+// user login routing
 app.post('/login', (req, res) => {
-
-  // check if the user doesn't exist in the datbase
   if (!req.body.email || !req.body.password) {
     return res.status(400).send(`Cannot leave email and password empty`);
   }
 
-  let userData = isUserInDatabase(req.body.email);
+  let userData = isUserEmailInDatabase(req.body.email);
   if (userData === false) {
-    return res.status(403).send(`User doesn't exist in database`);
+    return res.status(403).send(`User does not exist in database`);
   }
-
   // check if the password matches with the one stored in the datbase
   if (!bcrypt.compareSync(req.body.password,userData.password)) {
     return res.status(403).send(`Incorrect password`);
   }
-
   //set the appropriate cookie
-  res.cookie('user_id', userData.id);
+  res.sesssion.user_id = userData.id;
   res.redirect('/urls');
 });
-
-// logout user
-app.post('/logout', (req, res) => {
-  res.clearCookie('user_id');
-  res.redirect('/urls');
-});
-
-//hello
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
-
-//user registration
-app.get('/register', (req, res) => {
-  if (req.cookies.user_id) {
+// user registration page
+app.get('/register', (req,res) => {
+  // !!! subject to change
+  if (req.session.user_id) {
     res.redirect('/urls');
     return;
   }
-  const user = users[req.cookies.user_id];
-  const templateVars = { user };
+  const templateVars = {user: users[req.session.user_id]};
   res.render('register', templateVars);
 });
 
-app.post("/register", (req, res) => {
-
-  //email/password is empty
+// user registration routing
+app.post('/register', (req,res)=>{
+  // if bad input 
   if (req.body.email === '' || req.body.password === '') {
-    return res.status(400).send('Email or password is empty!')
+    return res.status(400).send(`Cannot leave email and password empty`);
   }
-
-  // user already exists
-  if (isUserInDatabase(req.body.email)) {
+  
+  // user already exists in datbase
+  if (isUserEmailInDatabase(req.body.email) !== false) {
     return res.status(400).send(`User already exists`);
   }
-
-  //set up a new user
-  let newUserID = generateRandomString();
-
-  users[newUserID] = {
-    id: newUserID,
+  
+  // setup new user
+  let newUserRandomID = generateRandomString();
+  users[newUserRandomID] = {
+    id: newUserRandomID,
     email: req.body.email,
     password: bcrypt.hashSync(req.body.password, 10)
   };
-  console.log(users);
-  res.cookie('user_id', newUserID)
+  res.cookie('user_id', newUserRandomID);
   res.redirect('/urls');
-})
+});
 
-//create new shortened url web form
-app.get("/urls/new", (req, res) => {
-  if (!users[req.cookies.user_id]) {
+// create a new shortened url web form
+app.get('/urls/new', (req,res) => {
+  if (!req.session.user_id) {
     res.redirect('/login');
     return;
   }
-  const templateVars = { user: users[req.cookies.user_id] };
+
+  const templateVars = {user: users[req.session.user_id]};
   res.render('urls_new', templateVars);
 });
 
-// list of stored urls
+// show the list of urls that is stored
 app.get("/urls", (req, res) => {
-  if (!users[req.cookies.user_id]) {
-    res.status(401).send("Please login or register")
+  if (!req.session.user_id) {
+    res.status(401).send('Please login or register');
     return;
   }
 
-  let usersURL = urlsForUser(req.cookies.user_id)
-  const templateVars = { urls: usersURL, user: users[req.cookies.user_id] };
+  let usersURL = urlsForUser(req.session.user_id);
+  const templateVars = { urls: usersURL, user: users[req.session.user_id] };
   res.render("urls_index", templateVars);
 });
 
-// add new url to database
-app.post("/urls", (req, res) => {
-  if (!users[req.cookies.user_id]) {
-    return res.status(403).send("Please login or register");
+// add new url to the database
+app.post('/urls', (req, res) => {
+  if (!req.session.user_id) {
+    return res.status(403).send('Unable to create TinyURl. Please login');
   }
+
   if (!req.body.longURL) {
     return res.status(400).send('Invalid URL');
   }
 
-  let id = generateRandomString();
-  urlDatabase[id] = {
+  let newShortURL = generateRandomString();
+  urlDatabase[newShortURL] = {
     longURL: req.body.longURL,
-    userID: req.cookies.user_id
-  }
-  res.redirect(`/urls/${id}`)
-})
+    userID: req.session.user_id
+  };
+  res.redirect(`/urls/${newShortURL}`);
+});
 
-//delete URL
-app.post("/urls/:id/delete", (req, res) => {
-  if (!users[req.cookies.user_id]) {
-    return res.status(403).send('Unable to delete Tiny URL');
-  }
+// delete the url
+app.post('/urls/:id/delete', (req, res)=> {
+  if (!req.session.user_id) {
+    return res.status(403).send('Unable to delete TinyURL');
+  } 
 
-  if (urlDatabase[req.params.id].userID !== req.cookies.user_id) {
-    return res.status(403).send('Unable to modify other users\' URLs');
+  if (urlDatabase[req.params.id].userID !== req.session.user_id) {
+    return res.status(403).send('Unable to delete modify another users Tiny URLs');
   }
-
   delete urlDatabase[req.params.id];
+  res.redirect('/urls');
+});
+
+// edit the url
+app.post('/urls/:id', (req, res) => {
+  if (!req.session.user_id) {
+    return res.status(403).send('Unable to edit Tiny URL');
+  }
+
+  if (urlDatabase[req.params.id].userID !== req.session.user_id) {
+    return res.status(403).send('Unable to delete modify another users Tiny URLs');
+  }
+
+  urlDatabase[req.params.id].longURL = req.body.longURL;
   res.redirect(`/urls`);
 });
 
-// edit URL
-app.post(`/urls/:id`, (req, res) => {
-  if (!users[req.cookies.user_id]) {
-    return res.status(403).send('Unable to edit URL')
-  }
-  if (urlDatabase[req.params.id].userID !== req.cookies.user_id) {
-    return res.status(403).send('Unable to modify other users\' URLs');
-  }
-  urlDatabase[req.params.id].longURL = req.body.longURL
-  res.redirect(`/urls`);
-});
-
-// showing the newly created link
+// re-route to display newly created link
 app.get("/urls/:id", (req, res) => {
-  if (!users[req.cookies.user_id]) {
-    return res.status(403).send('Please login');
+  if (!req.session.user_id) {
+    return res.status(403).send('Please Login');
+  }
+  if (!urlDatabase[req.params.id]) {
+    return res.status(404).send(`Provided tiny URL [${req.params.id}] not found in database`);
   }
 
-  if (!urlDatabase[req.params.id]) {
-    return res.status(404).send(`Provided Tiny URL [${req.params.id}] not found in database`);
-  }
-  if (urlDatabase[req.params.id].userID !== req.cookies.user_id) {
+  if (urlDatabase[req.params.id].userID !== req.session.user_id) {
     return res.status(403).send('Forbidden');
   }
 
-  const templateVars =
-  {
+  const templateVars = {
     id: req.params.id,
     longURL: urlDatabase[req.params.id].longURL,
-    user: users[req.cookies.user_id]
+    user: users[req.session.user_id]
   };
+  res.render("urls_show", templateVars);
 
-  res.render("urls_show", templateVars)
-})
-
-// redirects to new web form
+});
+// redirect to the new web form
 app.get("/u/:id", (req, res) => {
   if (!urlDatabase[req.params.id]) {
-    res.statusCode = 404;
-    res.statusMessage = 'Not Found';
-    return res.status(404).send(`Provided Tiny URL [${req.params.id}] not found in database`);
+    return res.status(404).send(`Provided tiny URL [${req.params.id}] not found in database`);
   }
   const longURL = urlDatabase[req.params.id].longURL;
   res.redirect(longURL);
 });
 
-// server listening on PORT
+
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Tiny app listening on port ${PORT}`);
 });
